f9a3ecab30890e817da00d0e501eb9056ea36b2d
williamrose <walk4839@gmail.com>
2023-06-04 21:49:57 +0200

    test

--- server.ts
+++ server.ts
@@ -1,3 +1,4 @@
+import http from 'http';
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { staticMiddleWare as graphQlStatic, router as graphQlRouter} from './staticRoutes/graphQl';
@@ -6,6 +7,7 @@ import { staticMiddleWare as contextStatic, router as contextRouter} from './sta
 dotenv.config();
 
 const app: Express = express();
+const httpServer = http.createServer(app);
 const port = process.env.HTTP_PORT || 8080;
 
 app.use('/graphql', graphQlStatic);
@@ -17,6 +19,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
