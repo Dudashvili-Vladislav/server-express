4379a57eb9c7ca6e0b61d1bed93c5af6aee5947a
williamrose <walk4839@gmail.com>
2023-06-04 21:40:46 +0200

    fix

--- staticRoutes/context.ts
+++ staticRoutes/context.ts
@@ -4,10 +4,11 @@ import path from 'path';
 const router = express.Router();
 
 const ContextStaticPath = process.env.CONTEXT_STATIC_PATH || '';
+const parentPath = __dirname.replace('/staticRoutes', '');
 
-router.use(express.static(path.join(__dirname, ContextStaticPath)));
+router.use(express.static(path.join(parentPath, ContextStaticPath)));
 router.get('*', (req: Request, res: Response) => {
-  res.sendFile(path.resolve(__dirname, `${ContextStaticPath}/index.html`));
+  res.sendFile(path.resolve(parentPath, `${ContextStaticPath}/index.html`));
 });
 
 export default router;
--- staticRoutes/graphQl.ts
+++ staticRoutes/graphQl.ts
@@ -4,10 +4,11 @@ import path from 'path';
 const router = express.Router();
 
 const graphQlStaticPath = process.env.GRAPH_QL_STATIC_PATH || '';
+const parentPath = __dirname.replace('/staticRoutes', '');
 
-router.use(express.static(path.join(__dirname, graphQlStaticPath)));
+router.use(express.static(path.join(parentPath, graphQlStaticPath)));
 router.get('*', (req: Request, res: Response) => {
-  res.sendFile(path.resolve(__dirname, `${graphQlStaticPath}/index.html`));
+  res.sendFile(path.resolve(parentPath, `${graphQlStaticPath}/index.html`));
 });
 
 export default router;
